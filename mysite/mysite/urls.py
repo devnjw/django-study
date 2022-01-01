dbba4b35d2d0cacb9a2aedad443726be215de929
devnjw <devnjw@gmail.com>
2022-01-02 01:29:25 +0900

    add rest-framework practice app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import urls
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include(urls)),
 ]
---
+++ mysite/rest/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+from . import views
+
+# path('login/', views.LoginView.as_view(template_name='rest_framework/login.html'), name='login'),
+# path('logout/', views.LogoutView.as_view(), name='logout'),
